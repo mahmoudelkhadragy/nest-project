b6134a3cad11751dc86b8d0918e106740317eda2
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2026-09-16 03:08:44 +0300

    setup database

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,33 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TweetModule } from './tweet/tweet.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from 'node_modules/@nestjs/typeorm/dist/typeorm.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, TweetModule, AuthModule],
+  imports: [
+    UsersModule,
+    TweetModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        entities: [],
+        synchronize: true,
+        host: configService.get('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get('DATABASE_USERNAME'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        autoLoadEntities: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
